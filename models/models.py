77f97744a8f414e87ab069a694f62d8690663886
Lizzie <48409053+Lizzie1234567@users.noreply.github.com>
2023-12-10 17:41:53 +0800

    Update models.py

--- models/models.py
+++ models/models.py
@@ -1,5 +1,5 @@
-from torch_geometric.nn import GCNConv, GATConv, SAGEConv, ChebConv,GIN,GINConv,global_mean_pool,Dropout
-from torch.nn import Module, Linear,Sequential,ReLU,ModuleList
+from torch_geometric.nn import GCNConv, GATConv, SAGEConv, ChebConv,GIN,GINConv,global_mean_pool
+from torch.nn import Module, Linear,Sequential,ReLU,ModuleList,Dropout
 from torch_geometric.nn.conv.gatv2_conv import GATv2Conv
 import torch.nn.functional as F
 
